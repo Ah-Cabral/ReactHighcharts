e1b098d4510e2faa30612e35fb582e19cfdcee2d
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-19 17:26:33 -0300

    Waterfall Chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WaterfallChart from "./Charts/WaterfallChart";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <WaterfallChart />
+    </div>
+  );
+}
