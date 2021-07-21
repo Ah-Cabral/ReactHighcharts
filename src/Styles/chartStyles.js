f62b9e97583bcd933314ad28a6ec60dfe7042f90
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-21 16:47:48 -0300

    Fixing Style

--- src/Styles/chartStyles.js
+++ src/Styles/chartStyles.js
@@ -16,12 +16,9 @@ import styled from 'styled-components'
   export const Card = styled.div`
     width: 100%;
     height: 100%;
-    padding: 2rem;
     margin: 3rem 0;
     border: 1px solid red;
     background: rgba( 255, 255, 255, 0.15 );
-    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.056);
-    backdrop-filter: blur( 0.0px );
     -webkit-backdrop-filter: blur( 0.0px );
     border-radius: 10px;
     border: 1px solid rgba(255, 255, 255, 0.145);
