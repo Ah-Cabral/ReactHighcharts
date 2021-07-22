9ff1b4f4ea63655be85b92cc76e71aa0434118c5
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-21 23:59:52 -0300

    Adding Another Chart And Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import AreaChart from "./Components/Charts/AreaChart/AreaChart";
 import WaterfallChart from "./Components/Charts/WaterfallChart/WaterfallChart";
 import StockChart from "./Components/Charts/StockChart/StockChart";
+import ColumnChart from "./Components/Charts/ColumnChart/ColumnChart";
+import Header from "./Components/Header/Header";
 
 import { GlobalStyle } from './Styles/GlobalStyle'
 
@@ -10,10 +13,12 @@ export default function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <div className='lineDiv'>
         <WaterfallChart />
         <AreaChart />
         <StockChart />
+        <ColumnChart />
       </div>
     </>
   );
