d8ab265202d4712d5f196598ff566dc2c5fa1b21
Arthur Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-07-22 00:11:25 -0300

    Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { MainHeader } from '../../Styles/chartStyles';
+
+const Header = () => {
+    return (
+        <MainHeader>
+            <p>Highcharts Project - By Arthur Cabral</p>
+        </MainHeader>
+    )
+}
+
+export default Header
