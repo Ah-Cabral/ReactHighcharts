f8fe3d43d1e553e392f05ea77f69649b9adf2eb0
Arthur Cabral <arthurcabral.dev@gmail.com>
2021-07-26 00:54:27 -0300

    Updating Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-import { MainHeader } from '../../Styles/chartStyles';
+import { MainHeader } from '../../Styles/HeaderStyle';
 
 const Header = () => {
     return (
         <MainHeader>
-            <p>Highcharts Project - By Arthur Cabral</p>
+            <div className='container'>
+                <p>Highcharts Project - By Arthur Cabral</p>
+                <a className='githubButton' href='https://github.com/CabralArthur' target='_blank' rel="noreferrer">
+                    <i class="fab fa-github"></i> My Github
+                </a>
+            </div>
         </MainHeader>
     )
 }
